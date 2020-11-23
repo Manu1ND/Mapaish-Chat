e612259ba6921c755a9d4004f17e6093fadd9c4a
Ishwar-Palav <ishwar.palav@sakec.ac.in>
2020-11-23 18:00:19 +0530

    Login and Register Done

---
+++ assets/reg.js
@@ -0,0 +1,43 @@
+$.validator.addMethod('regex', function (value, element, param) {
+    return this.optional(element) ||
+        value.match(typeof param == 'string' ? new RegExp(param) : param);
+});
+// form validation
+$(document).ready(function () {
+    $('#register').validate({
+        rules: {
+            mem_name: 'required',
+            username: 'required',
+            email: {
+                required: true,
+                email: true,
+            },
+
+            password: {
+                required: true,
+                regex: '^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$',
+            }
+        },
+        messages: {
+            mem_name: `<div class="alert alert-warning alert-dismissible fade show">
+                <strong>Warning!</strong> Full name is required.
+            </div>`,
+            username: `<div class="alert alert-warning alert-dismissible fade show">
+                <strong>Warning!</strong> Username is required.
+            </div>`,
+            email: `<div class="alert alert-warning alert-dismissible fade show">
+                <strong>Warning!</strong> Enter a valid email address.
+            </div>`,
+
+            password: {
+                regex: `<div class="alert alert-warning alert-dismissible fade show">
+                    <strong>Warning!</strong> Enter a valid password.
+                </div>`
+            },
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
+});
